398d6bd5226f3cacc31a7f1150cbdd751f797e64
megeee <115559755@qq.com>
2016-10-15 13:33:14 +0800

    update cookie

--- library.js
+++ library.js
@@ -1,8 +1,41 @@
 /*
  @整理一些自用的js代码 test
 */
-"use strict";
-var jeff = {
-    
+!(function(window,document){
 
-}
+var Jeff = {};
+    
+//cookie操作
+Jeff.cookie = {
+
+	del: function(name, path, domain){
+		document.cookie = name + "=" +
+			((path) ? "; path=" + path : "") +
+			((domain) ? "; domain=" + domain : "") +
+			"; expires=Thu, 01-Jan-70 00:00:01 GMT";
+	},
+
+	get: function(name){
+		var v = document.cookie.match('(?:^|;)\\s*' + name + '=([^;]*)');
+		return v ? decodeURIComponent(v[1]) : null;
+	},
+
+	set: function(name, value ,expires, path, domain){
+		var str = name + "=" + encodeURIComponent(value);
+		if (expires != null || expires != '') {
+			if (expires == 0) {expires = 100*365*24*60;}
+			var exp = new Date();
+			exp.setTime(exp.getTime() + expires*60*1000);
+			str += "; expires=" + exp.toGMTString();
+		}
+		if (path) {str += "; path=" + path;}
+		if (domain) {str += "; domain=" + domain;}
+		document.cookie = str;
+	}
+
+};
+
+window.jeff = Jeff;
+
+})(window,document)
+
