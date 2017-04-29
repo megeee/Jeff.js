68150789d71e609a603265b0c6a7c7477bb8b5e0
JeffGuo <85876521@qq.com>
2017-04-29 22:36:20 +0800

    update

--- Jeff.js
+++ Jeff.js
@@ -28,6 +28,30 @@
         
     }
     //字符串类
+
+    /*
+    * 实现utf8解码
+    */
+    Jeff.prototype.utf8_decode = function(str){
+        var tmp_arr = [],i = 0,ac = 0,c1 = 0,c2 = 0,c3 = 0;str += '';
+        while (i < str.length) {
+            c1 = str.charCodeAt(i);
+            if (c1 < 128) {
+                tmp_arr[ac++] = String.fromCharCode(c1);
+                i++;
+            } else if (c1 > 191 && c1 < 224) {       
+                c2 = str.charCodeAt(i + 1);
+                tmp_arr[ac++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
+                i += 2;
+            } else {
+                c2 = str.charCodeAt(i + 1);
+                c3 = str.charCodeAt(i + 2);
+                tmp_arr[ac++] = String.fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+        } 
+        return tmp_arr.join('');
+    }
     //URL类
     
 /*********************  URL类  *********************/
